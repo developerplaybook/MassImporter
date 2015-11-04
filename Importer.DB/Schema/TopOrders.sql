15cef1548c8084ec59fe554a97faa4904c7a52dc
Samuel Mensah <samuel.nimako-mensah@popagency.com>
2015-11-04 11:06:11 -0800

    added sql creation script

---
+++ Importer.DB/Schema/TopOrders.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[TopOrders] (
+    [NewOrderID]         INT           NOT NULL,
+    [NewCustomerId]      NVARCHAR (50) NULL,
+    [NewShippingAddress] NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([NewOrderID] ASC)
+);
